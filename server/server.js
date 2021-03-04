7b08ca730ba0bd6739dcf3ab2c6286c4c0138e3d
William Wassmann <wmwassmann@hotmail.com>
2021-03-03 18:41:39 -0800

    heroku test

--- server/server.js
+++ server/server.js
@@ -12,10 +12,14 @@ io.on('connection', (socket) => {
   console.log('connection confirmed');
   const id = socket.handshake.query.id
   socket.join(id)
+
+  // socket connects with the 'send-message' item
   socket.on('send-message', ({ recipients, text }) => {
     recipients.forEach(recipient => {
+      // const for the recipient
       const newRecipients = recipients.filter(r => r !== recipient)
       newRecipients.push(id)
+      // Socket then emits the messege from sender to the recipient
       socket.broadcast.to(recipient).emit('receive-message', {
         recipients: newRecipients, sender: id, text
       })
